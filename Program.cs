644cf3a82ca02a2f73cd915c8c24964ba5d3a5b9
RozbitiOkno <rozbitiokno@gmail.com>
2025-11-23 21:26:33 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
-﻿
+﻿//all by RozbitiOkno - funkcions and varibles
+
+ConsoleColor puvodníbarav1 = Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("int - only numbers 123");
+Console.ForegroundColor = puvodníbarav1;
+//int - only numbers 123
+
 int A = 100;
 System.Console.WriteLine("Hello World! " + A );
+
+ConsoleColor puvodníbarav2 = Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("if-else funkcion");
+Console.ForegroundColor = puvodníbarav2;
+//if-else funkcion
 if (A < 100) 
 {
     Console.WriteLine("true!");
@@ -10,20 +23,33 @@ else
     Console.WriteLine("test");
 }
 
+ConsoleColor puvodníbarav3= Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Double varible + WriteLine");
+Console.ForegroundColor = puvodníbarav3;
+//double - only numbers like 12.123
+
 double cena = 69.99;
 Console.WriteLine("Rohlíky " + cena + " int A " + A );
 
+ConsoleColor puvodníbarav4 = Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Void funkce");
+Console.ForegroundColor = puvodníbarav4;
+//void test funkcion
 void test()
 {
-    Console.WriteLine("TEST funkce! ");
+    Console.WriteLine("TEST funkcion! ");
 }
 
 test();
 
-ConsoleColor puvodniBarva = Console.ForegroundColor;
-Console.ForegroundColor = ConsoleColor.Red;
-Console.WriteLine("Switch funkce");
-Console.ForegroundColor = puvodniBarva;
+ConsoleColor puvodníbarav5 = Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Switch funkcion");
+Console.ForegroundColor = puvodníbarav5;
+//switch funkcion
+
 switch (A)
 
 {
@@ -36,5 +62,16 @@ switch (A)
          Console.WriteLine("Case 5"); 
     break;
 }
+
+List<int> intlist = new List<int>() {1,2,3}; //normal set value
+intlist.Add(5); //added value
+//Console.WriteLine(string.Join(", ", intlist)); //write to conslo
+
+//LOOPS! - verry important!
+foreach (int loop in intlist)
+{
+    Console.WriteLine(loop);
+}
+
 
  
